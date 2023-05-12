35a12c7c8155a14a1df6c76db88c399e29d93633
tuanvu041299 <tuanvu041299@gmail.com>
2023-05-12 11:18:23 +0700

    ss1_bai_tap

---
+++ ss1_database/bai_tap/ss1_bai_tap.sql
@@ -0,0 +1,14 @@
+create database ss1;
+use ss1;
+create table class(
+id int primary key,
+`name` varchar(45)
+);
+insert into class value(1,"C0223G1");
+create table teacher(
+id int primary key,
+`name` varchar(45),
+age int,
+country varchar(45)
+);
+insert into teacher value(1,"ChanhTv",40,"DaNang");
