e3655b2d809982a97c92f0d0eda31d1bf6be9d03
tuanvu041299 <tuanvu041299@gmail.com>
2023-05-17 17:07:55 +0700

    bài tập

---
+++ ss4_cac_ham_co_ban/bai_tap/quan_ly_sinh_vien.sql
@@ -0,0 +1,81 @@
+create database ss4_quan_ly_sinh_vien;
+use ss4_quan_ly_sinh_vien;
+
+CREATE TABLE class
+(
+    class_id   INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    class_name VARCHAR(60) NOT NULL,
+    start_date DATETIME    NOT NULL,
+    status    BIT
+);
+INSERT INTO class(class_name,start_date,status)
+VALUES ('A1', '2008-12-20', 1),
+ ('A2', '2008-12-22', 1),
+ ('B3', current_date, 0);
+
+
+CREATE TABLE student
+(
+    student_id   INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    student_name VARCHAR(30) NOT NULL,
+    address     VARCHAR(50),
+    phone       VARCHAR(20),
+    status      BIT,
+    class_id     INT         NOT NULL,
+    FOREIGN KEY (class_id) REFERENCES class (class_id)
+);
+INSERT INTO student (student_name, address, phone, status, class_id)
+VALUES ('Hung', 'Ha Noi', '0912113113', 1, 1),
+		('Hoa', 'Hai phong',null, 1, 1),
+		('Manh', 'HCM', '0123123123', 0, 2);
+ select*
+ from student
+ where substring_index(student_name," ",-1)   like "h%";
+ 
+CREATE TABLE subject
+(
+    sub_id   INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    sub_name VARCHAR(30) NOT NULL,
+    credit  TINYINT     NOT NULL DEFAULT 1 CHECK ( credit >= 1 ),
+    Status  BIT                  DEFAULT 1
+);
+
+INSERT INTO subject
+VALUES (1, 'CF', 5, 1),
+       (2, 'C', 6, 1),
+       (3, 'HDJ', 5, 1),
+       (4, 'RDBMS', 10, 1);
+
+CREATE TABLE mark
+(
+    mark_id    INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    sub_id     INT NOT NULL,
+    student_id INT NOT NULL,
+    mark FLOAT   DEFAULT 0 CHECK ( mark BETWEEN 0 AND 100),
+    exam_times TINYINT DEFAULT 1,
+    UNIQUE (sub_id, student_id),
+    FOREIGN KEY (sub_id) REFERENCES subject (sub_id),
+    FOREIGN KEY (student_id) REFERENCES student (student_id)
+);
+INSERT INTO mark (sub_id, student_id, mark, exam_times)
+VALUES (1, 1, 8, 1),
+       (1, 2, 10, 2),
+       (2, 1, 12, 1);
+       
+-- Hiển thị tất cả các thông tin môn học (bảng subject) có credit lớn nhất.
+select *
+from subject
+order by credit desc
+limit 1;
+-- Hiển thị các thông tin môn học có điểm thi lớn nhất.
+select *
+from mark
+where mark=(
+select  max(mark) 
+from mark
+);
+-- Hiển thị các thông tin sinh viên và điểm trung bình của mỗi sinh viên, xếp hạng theo thứ tự điểm giảm dần
+select student.*, avg(mark)
+from student
+join mark on student.student_id=mark.student_id
+group by student.student_id ;
